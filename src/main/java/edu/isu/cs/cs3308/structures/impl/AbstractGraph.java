725ccf06c0aa750fb71781b754b7e6adeb2777d3
harvaaro <harvaaro@isu.edu>
2019-04-23 15:18:52 -0600

    Now working on the rest of them.

--- src/main/java/edu/isu/cs/cs3308/structures/impl/AbstractGraph.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/AbstractGraph.java
@@ -2,12 +2,13 @@ package edu.isu.cs.cs3308.structures.impl;
 
 import edu.isu.cs.cs3308.structures.Graph;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public abstract class AbstractGraph<V,E> implements Graph<V,E> {
-	Map<V, List<E>> adjMap;
+	Map<V, List<V>> adjMap;
 
 
 
@@ -53,7 +54,120 @@ public abstract class AbstractGraph<V,E> implements Graph<V,E> {
 	 * @param e
 	 */
 	@Override
-	public void insertEdge(V u, V v, E e) {
+	public void insertEdge(V v, V u) {
+		if (v == null && u == null)
+			return;
+
+		if (adjMap.containsKey(v)) {
+			List<V> edges = adjMap.get(v);
+			if (!edges.contains(u))
+				edges.add(u);
+		}
+	}
+
+	/**
+	 * Returns an iteration of all the vertices of the graph
+	 */
+	@Override
+	public Iterator<Vertex<V, E>> vertices() {
+		return null;
+	}
+
+	/**
+	 * Returns an iteration of all edges of the graph
+	 */
+	@Override
+	public Iterator<Edge<E>> edges() {
+		return null;
+	}
+
+	/**
+	 * Returns the edge from vertex u to vertex v, if one exists, otherwise returns null.
+	 * For an undirected graph, there is no difference between getEdge(u, v) and getEdge(v, u)
+	 *
+	 * @param u
+	 * @param v
+	 */
+	@Override
+	public Edge<E> getEdge(Vertex<V, E> u, Vertex<V, E> v) {
+		return null;
+	}
+
+	/**
+	 * Returns an array containing the two endpoint vertices of edge e. If the graph is directed
+	 * the first vertex is the origin and the second is the destination.
+	 *
+	 * @param e
+	 */
+	@Override
+	public Vertex<V, E>[] endVertices(Edge<E> e) {
+		return new Vertex[0];
+	}
+
+	/**
+	 * For edge e incident to vertex v, returns the other vertex of the edge; an error occurs
+	 * if e is not incident to v.
+	 *
+	 * @param v
+	 * @param e
+	 */
+	@Override
+	public Vertex<V, E> opposite(Vertex<V, E> v, Edge<E> e) {
+		return null;
+	}
+
+	/**
+	 * Returns the number of outgoing edges from vertex v.
+	 *
+	 * @param v
+	 */
+	@Override
+	public int outDegree(Vertex<V, E> v) {
+		return 0;
+	}
+
+	/**
+	 * Returns the number of incoming edges to vertex v. For an undirected graph, this returns
+	 * the same value as does outDegree(v)
+	 *
+	 * @param v
+	 */
+	@Override
+	public int inDegree(Vertex<V, E> v) {
+		return 0;
+	}
+
+	/**
+	 * Returns an iteration of all outgoing edges from vertex v
+	 *
+	 * @param v
+	 */
+	@Override
+	public Iterator<Edge<E>> outgoingEdges(Vertex<V, E> v) {
+		return null;
+	}
+
+	/**
+	 * Returns an iteration of all incoming edges to vertex v. For an undirected graph, this
+	 * returns the same collection as does outgoingEdges(v)
+	 *
+	 * @param v
+	 */
+	@Override
+	public Iterator<Edge<E>> incomingEdges(Vertex<V, E> v) {
+		return null;
+	}
+
+	/**
+	 * Creates and returns a new Edge from vertex u to vertex v, storing element e; an error occurs
+	 * if there already exists an edge from u to v
+	 *
+	 * @param u
+	 * @param v
+	 * @param e
+	 */
+	@Override
+	public void insertEdge(Vertex<V, E> u, Vertex<V, E> v, E e) {
 
 	}
 
@@ -75,5 +189,37 @@ public abstract class AbstractGraph<V,E> implements Graph<V,E> {
 	@Override
 	public E removeEdge(Edge<E> e) {
 		return null;
+	}
+
+	/**
+	 * Removes vertex v and all its incident edges from the graph
+	 *
+	 * @param v
+	 */
+	@Override
+	public V removeVertex(V v) {
+		//TODO remove incoming edges
+
+		if (v != null && !adjMap.containsKey(v))
+			adjMap.remove(v);
+
+		return v;
+	}
+
+	/**
+	 * Removes edge e from the graph
+	 *
+	 * @param e
+	 */
+	@Override
+	public void removeEdge(V v, V u) {
+		if (v == null && u == null)
+			return;
+
+		if (adjMap.containsKey(v)) {
+			List<V> edges = adjMap.get(v);
+			if (edges.contains(u))
+				edges.remove(u);
+		}
 	}
 }
