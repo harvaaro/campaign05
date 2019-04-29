12680c146847c3ab2ccccfff659c01b239e02e29
harvaaro <harvaaro@isu.edu>
2019-04-28 20:30:09 -0600

    Debugging the PrintConfig, and the graph is not being made correctly. The edges I think are not being connected properly. Need to fix those creations I think.

--- src/main/java/edu/isu/cs/cs3308/structures/impl/FileParser.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/FileParser.java
@@ -53,29 +53,4 @@ public class FileParser {
 		return graph;
 	}
 
-	/*
-	void Parse(List<String> list) {
-		System.out.println("\nCurrent Network Configuration:");
-		System.out.println("-----------------------------");
-
-		for (String node : list) {
-			String[] split1 = node.split(":");
-			String[] split2 = split1[1].split(" ");
-
-			for (int i = 1; i < split2.length; i++) {
-				String[] split3 = split2[i].split("\\(");
-				split2[i] = split3[0];
-				if (split3.length > 1) {
-					String weight = split3[1].substring(0, split3[1].length() - 1);
-					System.out.println(split1[0] + "\t->\t" + split2[i] + "\t(" + weight + ")");
-				} else {
-					System.out.println(split1[0] + "\t->\t" + split2[i]);
-				}
-			}
-		}
-
-		System.out.println("\n-----------------------------");
-	}
-	*/
-
 }
--- src/main/java/edu/isu/cs/cs3308/structures/impl/ImplGraph.java
+++ src/main/java/edu/isu/cs/cs3308/structures/impl/ImplGraph.java
@@ -12,14 +12,29 @@ public class ImplGraph<V, E> extends AbstractGraph<V, E> {
 		System.out.println("\nCurrent Network Configuration:");
 		System.out.println("-----------------------------");
 
+		Iterator<V> vertices = this.vertices();
 		Iterator<Edge<V, E>> edges = this.edges();
-		for (int i = 0; i < numEdges(); i++) {
-			Edge<V, E> edge = edges.next();
-			V[] edgeVertices = this.endVertices(edge);
-			if (edge.getWeight() != null) {
-				System.out.println(edgeVertices[0] + "\t->\t" + edgeVertices[1] + "\t(" + edge.getWeight() + ")");
-			} else {
-				System.out.println(edgeVertices[0] + "\t->\t" + edgeVertices[1]);
+		for (int v = 0; v < numVertices(); v++) {
+			System.out.println("\nvert =\t" + v);
+			V vertex = vertices.next();
+			System.out.println("val =\t" + vertex.toString());
+			System.out.println("ins =\t" + this.inDegree(vertex));
+			System.out.println("outs =\t" + this.outDegree(vertex));
+			Iterator<Edge<V, E>> outEdges = this.outgoingEdges(vertex);
+			int outNum = this.outDegree(vertex);
+
+			for (int e = 0; e < outNum; e++) {
+				System.out.println("\nedge =\t" + e);
+				Edge<V, E> edge = outEdges.next();
+				System.out.println("src =\t" + edge.getSrc());
+				System.out.println("dest =\t" + edge.getDest());
+				System.out.println("pow =\t" + edge.getWeight());
+				V[] edgeVertices = this.endVertices(edge);
+				if (edge.getWeight() != null) {
+					System.out.println(edgeVertices[0] + "\t->\t" + edgeVertices[1] + "\t(" + edge.getWeight() + ")");
+				} else {
+					System.out.println(edgeVertices[0] + "\t->\t" + edgeVertices[1]);
+				}
 			}
 		}
 
